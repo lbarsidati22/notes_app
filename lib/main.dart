9efa79781327f0855f3665f6bd745040c055e68b
lbarsidati22 <lbarsidati575@gmail.com>
2024-09-04 12:48:15 +0000

    create Views && notesview && change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/home_page.dart';
+import 'package:notes_app/views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.dark(
+      theme: ThemeData(
+        brightness: Brightness.dark,
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const NotesView(),
     );
   }
 }
