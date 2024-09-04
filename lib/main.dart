355141dde6c20816422d70bc0bb495d2572757a1
lbarsidati22 <lbarsidati575@gmail.com>
2024-09-04 12:40:01 +0000

    theme home

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData.dark(
         useMaterial3: true,
       ),
       home: const HomePage(),
